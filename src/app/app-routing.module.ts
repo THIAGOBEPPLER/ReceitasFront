4a3cc66b04cd37230debe696131ef529778446cd
Thiago Beppler Santos <thiagobeppler@hotmial.com>
2020-10-14 20:35:21 -0300

    cadastro e busca

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { SugestaoComponent } from './sugestao/sugestao.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'sugestao', component: SugestaoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
