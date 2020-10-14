4e2d13cd657dc920d925ee45de8294897e4c7284
Thiago Beppler Santos <thiagobeppler@hotmial.com>
2020-10-14 17:55:29 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { SugestaoComponent } from './sugestao/sugestao.component';
+import { FormularioComponent } from './cadastro/formulario/formulario.component';
+import { BuscaComponent } from './cadastro/busca/busca.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CadastroComponent,
+    SugestaoComponent,
+    FormularioComponent,
+    BuscaComponent
   ],
   imports: [
     BrowserModule,
